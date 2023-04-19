0930f19d96efa907a6b6220a4ae77ec924e25831
Dxnax-RS <druizsalzar@gmail.com>
2023-04-19 13:23:16 -0600

    User, Boardroom and Department models created

--- database/migrations/2023_04_19_175607_create_users_table.php
+++ database/migrations/2023_04_19_175607_create_users_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
             $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
-            $table->timestamp('expires_at')->nullable();
+            $table->string('email');
+            $table->string('password');
+            $table->enum('role', ['CEO', 'Presidente', 'Vicepresidente', 'Jefe de departamento', 'Gerente', 'Supervisor', 'Empleado']);
+            $table->foreignId('department_id')->references('id')->on('departments');
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('users');
     }
 };
